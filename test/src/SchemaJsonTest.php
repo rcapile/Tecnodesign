6eb4884d2d29c858787a2953bbeef7090dee7def
Rodrigo Capilé <rcapile@gmail.com>
2019-02-26 16:32:04 -0300

    Schema export/validation

--- test/src/SchemaJsonTest.php
+++ test/src/SchemaJsonTest.php
@@ -2,6 +2,8 @@
 
 namespace TecnodesignTest;
 
+use Swaggest\JsonSchema\Schema as SwaggestSchema;
+
 require_once __DIR__ .'/../assets/models/SimpleModel.php';
 require_once __DIR__ .'/../assets/models/ComplexModel.php';
 
@@ -13,30 +15,52 @@ class SchemaJsonTest extends \PHPUnit_Framework_TestCase
 
     }
 
-    public function testLoadSchemaSimple()
+    public function testLoadSchema()
     {
-        $originalSchema = \SimpleModel::$schema;
-        $schema = new \Tecnodesign_Schema(new \SimpleModel);
         $class = new \ReflectionClass('Tecnodesign_Schema');
         //$schemaMetadata = $class->getStaticPropertyValue('meta');
         $schemaMetadata = $class->getStaticProperties()['meta'];
 
-        foreach ($schemaMetadata as $key=>$definition) {
-            $this->assertTrue(isset($schema[$key]), "\$schema has $key");
-            if (array_key_exists($key, $originalSchema)) {
-                // There should be not alias
-                if ($definition['alias']) {
-                   $this->assertNull($schema[$key], 'Alias should be null');
-                   $key = $definition['alias'];
-                }
-                $this->assertEquals(\SimpleModel::$schema[$key], $schema[$key],
-                    "\$schema $key is equal SimpleModel::\$schema");
+        foreach (['TecnodesignTest\\SimpleModel', 'TecnodesignTest\\ComplexModel'] as $className) {
+            $originalSchema = $className::$schema;
+            $schema = new \Tecnodesign_Schema(new $className());
+            foreach ($schemaMetadata as $key => $definition) {
+                $this->assertTrue(isset($schema[$key]), "\$schema has $key");
+                if (array_key_exists($key, $originalSchema)) {
+                    // There should be not alias
+                    if ($definition['alias']) {
+                        $this->assertNull($schema[$key], 'Alias should be null');
+                        $key = $definition['alias'];
+                    }
+                    $this->assertEquals($originalSchema[$key], $schema[$key],
+                        "\$schema $key is equal $className::\$schema");
 
-            } elseif($key === 'patternProperties') {
-                $this->assertEquals($schema[$key], ['/^_/' => ['type' => 'text']], "\$schema $key has default value");
-            } else{
-                $this->assertNull($schema[$key], "\$schema $key is null");
+                } elseif ($key === 'patternProperties') {
+                    $this->assertEquals($schema[$key], ['/^_/' => ['type' => 'text']],
+                        "\$schema $key has default value");
+                } else {
+                    $this->assertNull($schema[$key], "\$schema $key is null");
+                }
             }
+        }
+    }
+
+    public function testToJson()
+    {
+        $class = new \ReflectionClass('Tecnodesign_Schema');
+        //$schemaMetadata = $class->getStaticPropertyValue('meta');
+        $schemaMetadata = $class->getStaticProperties()['meta'];
+
+        foreach (['TecnodesignTest\\SimpleModel', 'TecnodesignTest\\ComplexModel'] as $className) {
+            $schema = new \Tecnodesign_Schema(new $className());
+            $json = $schema->toJson();
+            $jsonSchema = $schema->getJsonSchema();
+
+            $this->assertInternalType('string', $json);
+            $this->assertInternalType('string', $jsonSchema);
+
+            $schemaValidate = SwaggestSchema::import(json_decode($jsonSchema));
+            $schemaValidate->in((object)json_decode($json, JSON_OBJECT_AS_ARRAY));
         }
     }
 }
